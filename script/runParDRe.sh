f6d5258f9aa4f6323eaf699ea3d0660646b5745b
Yuansheng Liu <yyuanshengliu@gmail.com>
2020-08-25 09:32:15 +0800

    modify scripts to script

---
+++ script/runParDRe.sh
@@ -0,0 +1,29 @@
+nohup bash -c '/usr/bin/time -v mpirun -n 24 ./ParDRe -l 10 -i SRR921889.fastq -o SRR921889_m_l10_0.fastq -m 0' </dev/null 1> SRR921889_rm_0_l10.pardre.out 2> SRR921889_rm_0_l10.pardre.err
+nohup bash -c '/usr/bin/time -v mpirun -n 24 ./ParDRe -l 10 -i SRR921889.fastq -o SRR921889_m_l10_1.fastq -m 1' </dev/null 1> SRR921889_rm_1_l10.pardre.out 2> SRR921889_rm_1_l10.pardre.err
+nohup bash -c '/usr/bin/time -v mpirun -n 24 ./ParDRe -l 10 -i SRR921889.fastq -o SRR921889_m_l10_2.fastq -m 2' </dev/null 1> SRR921889_rm_2_l10.pardre.out 2> SRR921889_rm_2_l10.pardre.err
+nohup bash -c '/usr/bin/time -v mpirun -n 24 ./ParDRe -l 10 -i SRR921889.fastq -o SRR921889_m_l10_3.fastq -m 3' </dev/null 1> SRR921889_rm_3_l10.pardre.out 2> SRR921889_rm_3_l10.pardre.err
+
+nohup bash -c '/usr/bin/time -v mpirun -n 24 ./ParDRe -l 20 -i SRR921889.fastq -o SRR921889_m_l20_0.fastq -m 0' </dev/null 1> SRR921889_rm_0_l20.pardre.out 2> SRR921889_rm_0_l20.pardre.err
+nohup bash -c '/usr/bin/time -v mpirun -n 24 ./ParDRe -l 20 -i SRR921889.fastq -o SRR921889_m_l20_1.fastq -m 1' </dev/null 1> SRR921889_rm_1_l20.pardre.out 2> SRR921889_rm_1_l20.pardre.err
+nohup bash -c '/usr/bin/time -v mpirun -n 24 ./ParDRe -l 20 -i SRR921889.fastq -o SRR921889_m_l20_2.fastq -m 2' </dev/null 1> SRR921889_rm_2_l20.pardre.out 2> SRR921889_rm_2_l20.pardre.err
+nohup bash -c '/usr/bin/time -v mpirun -n 24 ./ParDRe -l 20 -i SRR921889.fastq -o SRR921889_m_l20_3.fastq -m 3' </dev/null 1> SRR921889_rm_3_l20.pardre.out 2> SRR921889_rm_3_l20.pardre.err
+
+nohup bash -c '/usr/bin/time -v mpirun -n 24 ./ParDRe -l 10 -i SRR948355_1.fastq -p SRR948355_2.fastq -o SRR948355_rm_l10_0.fastq.1 -r SRR948355_rm_l10_0.fastq.2 -m 0' </dev/null 1> SRR948355_rm_0_l10.pardre.pe.out 2> SRR948355_rm_0_l10.pardre.pe.err
+nohup bash -c '/usr/bin/time -v mpirun -n 24 ./ParDRe -l 10 -i SRR948355_1.fastq -p SRR948355_2.fastq -o SRR948355_rm_l10_1.fastq.1 -r SRR948355_rm_l10_1.fastq.2 -m 1' </dev/null 1> SRR948355_rm_1_l10.pardre.pe.out 2> SRR948355_rm_1_l10.pardre.pe.err
+nohup bash -c '/usr/bin/time -v mpirun -n 24 ./ParDRe -l 10 -i SRR948355_1.fastq -p SRR948355_2.fastq -o SRR948355_rm_l10_2.fastq.1 -r SRR948355_rm_l10_2.fastq.2 -m 2' </dev/null 1> SRR948355_rm_2_l10.pardre.pe.out 2> SRR948355_rm_2_l10.pardre.pe.err
+nohup bash -c '/usr/bin/time -v mpirun -n 24 ./ParDRe -l 10 -i SRR948355_1.fastq -p SRR948355_2.fastq -o SRR948355_rm_l10_3.fastq.1 -r SRR948355_rm_l10_3.fastq.2 -m 3' </dev/null 1> SRR948355_rm_3_l10.pardre.pe.out 2> SRR948355_rm_3_l10.pardre.pe.err
+
+nohup bash -c '/usr/bin/time -v mpirun -n 24 ./ParDRe -l 20 -i SRR948355_1.fastq -p SRR948355_2.fastq -o SRR948355_rm_l20_0.fastq.1 -r SRR948355_rm_l20_0.fastq.2 -m 0' </dev/null 1> SRR948355_rm_0_l20.pardre.pe.out 2> SRR948355_rm_0_l20.pardre.pe.err
+nohup bash -c '/usr/bin/time -v mpirun -n 24 ./ParDRe -l 20 -i SRR948355_1.fastq -p SRR948355_2.fastq -o SRR948355_rm_l20_1.fastq.1 -r SRR948355_rm_l20_1.fastq.2 -m 1' </dev/null 1> SRR948355_rm_1_l20.pardre.pe.out 2> SRR948355_rm_1_l20.pardre.pe.err
+nohup bash -c '/usr/bin/time -v mpirun -n 24 ./ParDRe -l 20 -i SRR948355_1.fastq -p SRR948355_2.fastq -o SRR948355_rm_l20_2.fastq.1 -r SRR948355_rm_l20_2.fastq.2 -m 2' </dev/null 1> SRR948355_rm_2_l20.pardre.pe.out 2> SRR948355_rm_2_l20.pardre.pe.err
+nohup bash -c '/usr/bin/time -v mpirun -n 24 ./ParDRe -l 20 -i SRR948355_1.fastq -p SRR948355_2.fastq -o SRR948355_rm_l20_3.fastq.1 -r SRR948355_rm_l20_3.fastq.2 -m 3' </dev/null 1> SRR948355_rm_3_l20.pardre.pe.out 2> SRR948355_rm_3_l20.pardre.pe.err
+
+nohup bash -c '/usr/bin/time -v mpirun -n 24 ./ParDRe -l 10 -i SRR377645.fastq -o SRR377645_m_l10_0.fastq -m 0' </dev/null 1> SRR377645_rm_0_l10.pardre.out 2> SRR377645_rm_0_l10.pardre.err
+nohup bash -c '/usr/bin/time -v mpirun -n 24 ./ParDRe -l 10 -i SRR377645.fastq -o SRR377645_m_l10_1.fastq -m 1' </dev/null 1> SRR377645_rm_1_l10.pardre.out 2> SRR377645_rm_1_l10.pardre.err
+nohup bash -c '/usr/bin/time -v mpirun -n 24 ./ParDRe -l 10 -i SRR377645.fastq -o SRR377645_m_l10_2.fastq -m 2' </dev/null 1> SRR377645_rm_2_l10.pardre.out 2> SRR377645_rm_2_l10.pardre.err
+nohup bash -c '/usr/bin/time -v mpirun -n 24 ./ParDRe -l 10 -i SRR377645.fastq -o SRR377645_m_l10_3.fastq -m 3' </dev/null 1> SRR377645_rm_3_l10.pardre.out 2> SRR377645_rm_3_l10.pardre.err
+
+nohup bash -c '/usr/bin/time -v mpirun -n 24 ./ParDRe -l 20 -i SRR377645.fastq -o SRR377645_m_l20_0.fastq -m 0' </dev/null 1> SRR377645_rm_0_l20.pardre.out 2> SRR377645_rm_0_l20.pardre.err
+nohup bash -c '/usr/bin/time -v mpirun -n 24 ./ParDRe -l 20 -i SRR377645.fastq -o SRR377645_m_l20_1.fastq -m 1' </dev/null 1> SRR377645_rm_1_l20.pardre.out 2> SRR377645_rm_1_l20.pardre.err
+nohup bash -c '/usr/bin/time -v mpirun -n 24 ./ParDRe -l 20 -i SRR377645.fastq -o SRR377645_m_l20_2.fastq -m 2' </dev/null 1> SRR377645_rm_2_l20.pardre.out 2> SRR377645_rm_2_l20.pardre.err
+nohup bash -c '/usr/bin/time -v mpirun -n 24 ./ParDRe -l 20 -i SRR377645.fastq -o SRR377645_m_l20_3.fastq -m 3' </dev/null 1> SRR377645_rm_3_l20.pardre.out 2> SRR377645_rm_3_l20.pardre.err
---
+++ script/runminirmd.sh
@@ -0,0 +1,30 @@
+nohup bash -c '/usr/bin/time -v ./minirmd -i SRR921889.fastq -o SRR921889_rm_0_minirmd.fastq -d 0' </dev/null 1> SRR921889_rm_0_minirmd.out 2> SRR921889_rm_0_minirmd.err
+nohup bash -c '/usr/bin/time -v ./minirmd -i SRR921889.fastq -o SRR921889_rm_1_minirmd.fastq -d 1' </dev/null 1> SRR921889_rm_1_minirmd.out 2> SRR921889_rm_1_minirmd.err
+nohup bash -c '/usr/bin/time -v ./minirmd -i SRR921889.fastq -o SRR921889_rm_2_minirmd.fastq -d 2' </dev/null 1> SRR921889_rm_2_minirmd.out 2> SRR921889_rm_2_minirmd.err
+nohup bash -c '/usr/bin/time -v ./minirmd -i SRR921889.fastq -o SRR921889_rm_3_minirmd.fastq -d 3' </dev/null 1> SRR921889_rm_3_minirmd.out 2> SRR921889_rm_3_minirmd.err
+
+nohup bash -c '/usr/bin/time -v ./minirmd -i SRR948355_1.fastq -f SRR948355_2.fastq -o SRR948355_pe_rm_0_minirmd.fastq -d 0' </dev/null 1> SRR948355_pe_rm_0_minirmd.out 2> SRR948355_pe_rm_0_minirmd.err
+nohup bash -c '/usr/bin/time -v ./minirmd -i SRR948355_1.fastq -f SRR948355_2.fastq -o SRR948355_pe_rm_1_minirmd.fastq -d 1' </dev/null 1> SRR948355_pe_rm_1_minirmd.out 2> SRR948355_pe_rm_1_minirmd.err
+nohup bash -c '/usr/bin/time -v ./minirmd -i SRR948355_1.fastq -f SRR948355_2.fastq -o SRR948355_pe_rm_2_minirmd.fastq -d 2' </dev/null 1> SRR948355_pe_rm_2_minirmd.out 2> SRR948355_pe_rm_2_minirmd.err
+nohup bash -c '/usr/bin/time -v ./minirmd -i SRR948355_1.fastq -f SRR948355_2.fastq -o SRR948355_pe_rm_3_minirmd.fastq -d 3' </dev/null 1> SRR948355_pe_rm_3_minirmd.out 2> SRR948355_pe_rm_3_minirmd.err
+
+nohup bash -c '/usr/bin/time -v ./minirmd -i SRR377645.fastq -o SRR377645_rm_0_minirmd.fastq -d 0' </dev/null 1> SRR377645_rm_0_minirmd.out 2> SRR377645_rm_0_minirmd.err
+nohup bash -c '/usr/bin/time -v ./minirmd -i SRR377645.fastq -o SRR377645_rm_1_minirmd.fastq -d 1' </dev/null 1> SRR377645_rm_1_minirmd.out 2> SRR377645_rm_1_minirmd.err
+nohup bash -c '/usr/bin/time -v ./minirmd -i SRR377645.fastq -o SRR377645_rm_2_minirmd.fastq -d 2' </dev/null 1> SRR377645_rm_2_minirmd.out 2> SRR377645_rm_2_minirmd.err
+nohup bash -c '/usr/bin/time -v ./minirmd -i SRR377645.fastq -o SRR377645_rm_3_minirmd.fastq -d 3' </dev/null 1> SRR377645_rm_3_minirmd.out 2> SRR377645_rm_3_minirmd.err
+
+# if removing duplicates on RC strand
+nohup bash -c '/usr/bin/time -v ./minirmd -i SRR921889.fastq -o SRR921889_rm_0_minirmd.rc.fastq -d 0 -r' </dev/null 1> SRR921889_rm_0_minirmd.rc.out 2> SRR921889_rm_0_minirmd.rc.err
+nohup bash -c '/usr/bin/time -v ./minirmd -i SRR921889.fastq -o SRR921889_rm_1_minirmd.rc.fastq -d 1 -r' </dev/null 1> SRR921889_rm_1_minirmd.rc.out 2> SRR921889_rm_1_minirmd.rc.err
+nohup bash -c '/usr/bin/time -v ./minirmd -i SRR921889.fastq -o SRR921889_rm_2_minirmd.rc.fastq -d 2 -r' </dev/null 1> SRR921889_rm_2_minirmd.rc.out 2> SRR921889_rm_2_minirmd.rc.err
+nohup bash -c '/usr/bin/time -v ./minirmd -i SRR921889.fastq -o SRR921889_rm_3_minirmd.rc.fastq -d 3 -r' </dev/null 1> SRR921889_rm_3_minirmd.rc.out 2> SRR921889_rm_3_minirmd.rc.err
+
+nohup bash -c '/usr/bin/time -v ./minirmd -i SRR948355_1.fastq -f SRR948355_2.fastq -o SRR948355_pe_rm_0_minirmd.rc.fastq -d 0 -r' </dev/null 1> SRR948355_pe_rm_0_minirmd.rc.out 2> SRR948355_pe_rm_0_minirmd.rc.err
+nohup bash -c '/usr/bin/time -v ./minirmd -i SRR948355_1.fastq -f SRR948355_2.fastq -o SRR948355_pe_rm_1_minirmd.rc.fastq -d 1 -r' </dev/null 1> SRR948355_pe_rm_1_minirmd.rc.out 2> SRR948355_pe_rm_1_minirmd.rc.err
+nohup bash -c '/usr/bin/time -v ./minirmd -i SRR948355_1.fastq -f SRR948355_2.fastq -o SRR948355_pe_rm_2_minirmd.rc.fastq -d 2 -r' </dev/null 1> SRR948355_pe_rm_2_minirmd.rc.out 2> SRR948355_pe_rm_2_minirmd.rc.err
+nohup bash -c '/usr/bin/time -v ./minirmd -i SRR948355_1.fastq -f SRR948355_2.fastq -o SRR948355_pe_rm_3_minirmd.rc.fastq -d 3 -r' </dev/null 1> SRR948355_pe_rm_3_minirmd.rc.out 2> SRR948355_pe_rm_3_minirmd.rc.err
+
+nohup bash -c '/usr/bin/time -v ./minirmd -i SRR377645.fastq -o SRR377645_rm_0_minirmd.rc.fastq -d 0 -r' </dev/null 1> SRR377645_rm_0_minirmd.rc.out 2> SRR377645_rm_0_minirmd.rc.err
+nohup bash -c '/usr/bin/time -v ./minirmd -i SRR377645.fastq -o SRR377645_rm_1_minirmd.rc.fastq -d 1 -r' </dev/null 1> SRR377645_rm_1_minirmd.rc.out 2> SRR377645_rm_1_minirmd.rc.err
+nohup bash -c '/usr/bin/time -v ./minirmd -i SRR377645.fastq -o SRR377645_rm_2_minirmd.rc.fastq -d 2 -r' </dev/null 1> SRR377645_rm_2_minirmd.rc.out 2> SRR377645_rm_2_minirmd.rc.err
+nohup bash -c '/usr/bin/time -v ./minirmd -i SRR377645.fastq -o SRR377645_rm_3_minirmd.rc.fastq -d 3 -r' </dev/null 1> SRR377645_rm_3_minirmd.rc.out 2> SRR377645_rm_3_minirmd.rc.err
