f2b6cf7b9ef3ec0fabe18a99d735b8eb4709c020
Yuansheng Liu <yyuanshengliu@gmail.com>
2020-08-25 09:36:15 +0800

    submit

--- script/runBioSeqZip.sh
+++
@@ -1,3 +0,0 @@
-nohup bash -c '/usr/bin/time -v ./bioseqzip_collapse -i SRR921889.fastq -f fastq -t 24' </dev/null 1> SRR921889.out 2> SRR921889.err
-nohup bash -c '/usr/bin/time -v ./bioseqzip_collapse -i SRR948355_1.fastq -p SRR948355_2.fastq -f fastq -t 24' </dev/null 1> SRR948355.out 2> SRR948355.err
-nohup bash -c '/usr/bin/time -v ./bioseqzip_collapse -i SRR377645.fastq -f fastq -t 24' </dev/null 1> SRR377645.out 2> SRR377645.err
